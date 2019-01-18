5d6eedced2028a4dc4b64ae07a0b22aae50b2bdc
mbuie <mbuie@cityofmesquite.com>
2019-01-18 15:27:44 -0600

    added instance of accelerometer

--- app/src/main/java/com/northbridgeanalytics/mysensors/MainActivity.java
+++ app/src/main/java/com/northbridgeanalytics/mysensors/MainActivity.java
@@ -1,15 +1,25 @@
 package com.northbridgeanalytics.mysensors;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get an instance to the accelerometer.
+        this.sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        this.accelerometer = this.sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
-    
+
 }
