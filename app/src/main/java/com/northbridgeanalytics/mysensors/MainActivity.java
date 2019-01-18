323520bde035dfaf090cb258fadbdea391bcd58e
mbuie <mbuie@cityofmesquite.com>
2019-01-18 17:00:28 -0600

    returns x, y, z

--- app/src/main/java/com/northbridgeanalytics/mysensors/MainActivity.java
+++ app/src/main/java/com/northbridgeanalytics/mysensors/MainActivity.java
@@ -2,14 +2,20 @@ package com.northbridgeanalytics.mysensors;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-    private SensorManager mSensorManager;
-    private Sensor mSensor;
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static final String TAG="MyMessage";
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private long lastUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,46 @@ public class MainActivity extends AppCompatActivity {
         // Get an instance to the accelerometer.
         this.sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         this.accelerometer = this.sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer , SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                Log.i(TAG, "x-value: " + x + " y-value: " + y + " z-value: " + z);
+
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
 
 
 }
