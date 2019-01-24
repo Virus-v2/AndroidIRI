9a26735684c7143e5fbf2bb27401fba6e28f1d69
Mark Buie <markebuie@gmail.com>
2019-01-23 18:23:07 -0600

    rotation matrix tutorial.

--- app/src/main/java/com/northbridgeanalytics/mysensors/MainActivity.java
+++ app/src/main/java/com/northbridgeanalytics/mysensors/MainActivity.java
@@ -1,8 +1,10 @@
 package com.northbridgeanalytics.mysensors;
 
 //https://code.tutsplus.com/tutorials/using-the-accelerometer-on-android--mobile-22125
+//https://google-developer-training.gitbooks.io/android-developer-advanced-course-practicals/content/unit-1-expand-the-user-experience/lesson-3-sensors/3-2-p-working-with-sensor-based-orientation/3-2-p-working-with-sensor-based-orientation.html
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,97 +12,138 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.Arrays;
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private static final String TAG="MyMessage";
-    private SensorManager sensorManager;
-    private Sensor accelerometer;
-    private Sensor rotation;
-    private long accelLastUpdate;
-    private long rotationLastUpdate;
-    private final float[] mRotationMatrix = new float[16];
+public class MainActivity extends AppCompatActivity
+        implements SensorEventListener {
 
+    // System sensor manager instance.
+    private SensorManager mSensorManager;
+
+    // Accelerometer and magnetometer sensors, as retrieved from the
+    // sensor manager.
+    private Sensor mSensorAccelerometer;
+    private Sensor mSensorMagnetometer;
+
+    // TextViews to display current sensor values.
+    private TextView mTextSensorAzimuth;
+    private TextView mTextSensorPitch;
+    private TextView mTextSensorRoll;
+
+    // Very small values for the accelerometer (on all three axes) should
+    // be interpreted as 0. This value is the amount of acceptable
+    // non-zero drift.
+    private static final float VALUE_DRIFT = 0.05f;
+
+    private float[] mAccelerometerData = new float[3];
+    private float[] mMagnetometerData = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get an instance to the accelerometer.
-        this.sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        this.accelerometer = this.sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        this.rotation = this.sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        sensorManager.registerListener(this, accelerometer , SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, rotation , SensorManager.SENSOR_DELAY_NORMAL);
+        // Lock the orientation to portrait (for now)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        mTextSensorAzimuth = (TextView) findViewById(R.id.value_azimuth);
+        mTextSensorPitch = (TextView) findViewById(R.id.value_pitch);
+        mTextSensorRoll = (TextView) findViewById(R.id.value_roll);
+
+        // Get accelerometer and magnetometer sensors from the sensor manager.
+        // The getDefaultSensor() method returns null if the sensor
+        // is not available on the device.
+        mSensorManager = (SensorManager) getSystemService(
+                Context.SENSOR_SERVICE);
+        mSensorAccelerometer = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER);
+        mSensorMagnetometer = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    /**
+     * Listeners for the sensors are registered in this callback so that
+     * they can be unregistered in onStop().
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Listeners for the sensors are registered in this callback and
+        // can be unregistered in onStop().
+        //
+        // Check to ensure sensors are available before registering listeners.
+        // Both listeners are registered with a "normal" amount of delay
+        // (SENSOR_DELAY_NORMAL).
+        if (mSensorAccelerometer != null) {
+            mSensorManager.registerListener(this, mSensorAccelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorMagnetometer != null) {
+            mSensorManager.registerListener(this, mSensorMagnetometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Unregister all sensor listeners in this callback so they don't
+        // continue to use resources when the app is stopped.
+        mSensorManager.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        Sensor mySensor = sensorEvent.sensor;
-        mRotationMatrix[ 0] = 1;
-        mRotationMatrix[ 4] = 1;
-        mRotationMatrix[ 8] = 1;
-        mRotationMatrix[12] = 1;
+        int sensorType = sensorEvent.sensor.getType();
 
-        Log.i(TAG, "array: " + Arrays.toString(mRotationMatrix));
-
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-
-            long curTime = System.currentTimeMillis();
-
-            if ((curTime - accelLastUpdate) > 1000) {
-                long diffTime = (curTime - accelLastUpdate);
-                accelLastUpdate = curTime;
-
-//                Log.i(TAG, "ax: " + x + " ay: " + y + " az: " + z);
-
-            }
+        switch (sensorType) {
+            case Sensor.TYPE_ACCELEROMETER:
+                mAccelerometerData = sensorEvent.values.clone();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mMagnetometerData = sensorEvent.values.clone();
+                break;
+            default:
+                return;
         }
 
-        if (mySensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
+        float[] rotationMatrix = new float[9];
+        boolean rotationOK = SensorManager.getRotationMatrix(rotationMatrix,
+                null, mAccelerometerData, mMagnetometerData);
 
-            SensorManager.getRotationMatrixFromVector(
-                    mRotationMatrix , sensorEvent.values);
-
-
-            long curTime = System.currentTimeMillis();
-
-            if ((curTime - rotationLastUpdate) > 5000) {
-                long diffTime = (curTime - rotationLastUpdate);
-                rotationLastUpdate = curTime;
-
-                Log.i(TAG, "matrix: " + Arrays.toString(mRotationMatrix));
-//                Log.i(TAG, "rx: " + x + " ry: " + y + " rz: " + z);
-
-            }
+        float orientationValues[] = new float[3];
+        if (rotationOK) {
+            SensorManager.getOrientation(rotationMatrix, orientationValues);
         }
+
+        float azimuth = orientationValues[0];
+        float pitch = orientationValues[1];
+        float roll = orientationValues[2];
+
+        mTextSensorAzimuth.setText(getResources().getString(
+                R.string.value_format, azimuth));
+        mTextSensorPitch.setText(getResources().getString(
+                R.string.value_format, pitch));
+        mTextSensorRoll.setText(getResources().getString(
+                R.string.value_format, roll));
+
+
+
 
     }
 
+    /**
+     * Must be implemented to satisfy the SensorEventListener interface;
+     * unused in this app.
+     */
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
 
     }
-
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
-    }
-
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-
-
 }
