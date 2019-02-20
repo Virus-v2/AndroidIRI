bb1f7bcf545054eb232e57a296e1a6b8bbe765c5
mbuie <mbuie@cityofmesquite.com>
2019-02-20 11:01:15 -0600

    initial commit

---
+++ app/src/main/java/utils/VectorAlgebra.java
@@ -0,0 +1,69 @@
+package utils;
+
+import android.hardware.SensorManager;
+
+public class VectorAlgebra {
+
+    public static float[] earthAccelerometer(float[] accelerometer, float[] magnetometer, float[] gravity, SensorManager sensorManager) {
+        float[] phoneAcceleration = new float[4];
+        phoneAcceleration[0] = accelerometer[0];
+        phoneAcceleration[1] = accelerometer[1];
+        phoneAcceleration[2] = accelerometer[2];
+        phoneAcceleration[3] = 0;
+
+        // Change the device relative acceleration values to earth relative values
+        // X axis -> East
+        // Y axis -> North Pole
+        // Z axis -> Sky
+
+        float[] R = new float[16], I = new float[16], earthAcceleration = new float[16];
+
+        sensorManager.getRotationMatrix(R, I, gravity, magnetometer);
+
+        float[] inv = new float[16];
+
+        android.opengl.Matrix.invertM(inv, 0, R, 0);
+        android.opengl.Matrix.multiplyMV(earthAcceleration, 0, inv, 0, phoneAcceleration, 0);
+
+        return earthAcceleration;
+
+    }
+
+
+    public static double[] radiansToDegrees(float[] inputRadians) {
+
+        double[] outputDegrees = new double[inputRadians.length];
+
+        for (int i=0; i < inputRadians.length; i++) {
+            outputDegrees[i] = inputRadians[i] * (180/Math.PI);
+        }
+
+        return outputDegrees;
+    }
+
+    public static float[] phoneOrientation(float[] accelerometer, float[] magnetometer, SensorManager sensorManager) {
+
+        // Empty Float array to hold the rotation matrix.
+        float[] rotationMatrix = new float[9];
+        // Empty Float array to hold the azimuth, pitch, and roll.
+        float orientationValues[] = new float[3];
+
+        // Not sure exactly how this works, but populates the matrix with the input data. rotationOK returns true if the
+        // .getRotationMatrix method is successful.
+        // "You can transform any vector from the phone's coordinate system to the Earth's coordinate system by
+        // multiplying it with the rotation matrix."
+        boolean rotationOK = sensorManager.getRotationMatrix(rotationMatrix,
+          null, accelerometer, magnetometer);
+
+        // If the getRotationMatrix method is successful run the following code,
+        // TODO Do I need this at all?.
+        if (rotationOK) {
+
+            sensorManager.getOrientation(rotationMatrix, orientationValues);
+
+        }
+
+        return orientationValues;
+    }
+
+}
