aec0347a09efa7b9de1a05aae9762a7e759b4fee
Mark Buie <markebuie@gmail.com>
2019-03-09 17:46:45 -0600

    initial commit

---
+++ app/src/main/java/SurfaceDoctor/SurfaceDoctorPoint.java
@@ -0,0 +1,25 @@
+package SurfaceDoctor;
+
+public class SurfaceDoctorPoint {
+    public static float x;
+    public static float y;
+    public static float z;
+    public static long tStart;
+    public static long tStop;
+
+    public long getTimeDiff() {
+        return tStop - tStart;
+    }
+
+    public float getIRIofX() {
+        return x * getTimeDiff() * getTimeDiff();
+    }
+
+    public float getIRIofY() {
+        return y * getTimeDiff() * getTimeDiff();
+    }
+
+    public float getIRIofZ() {
+        return z * getTimeDiff() * getTimeDiff();
+    }
+}
