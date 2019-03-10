0e104b0f5281c3d30d04f0f7dd65cfec7388dd5b
Mark Buie <markebuie@gmail.com>
2019-03-09 19:45:21 -0600

    added constructors.

--- app/src/main/java/SurfaceDoctor/SurfaceDoctorPoint.java
+++ app/src/main/java/SurfaceDoctor/SurfaceDoctorPoint.java
@@ -7,17 +7,30 @@ public class SurfaceDoctorPoint {
     public static long tStart;
     public static long tStop;
 
+
+    public SurfaceDoctorPoint(float accelerometerX, float accelerometerY, float accelerometerZ, long startTime, long stopTime) {
+        x = accelerometerX;
+        y = accelerometerY;
+        z = accelerometerZ;
+        tStart = startTime;
+        tStop = stopTime;
+    }
+
+
     public long getTimeDiff() {
         return tStop - tStart;
     }
+
 
     public float getIRIofX() {
         return x * getTimeDiff() * getTimeDiff();
     }
 
+
     public float getIRIofY() {
         return y * getTimeDiff() * getTimeDiff();
     }
+    
 
     public float getIRIofZ() {
         return z * getTimeDiff() * getTimeDiff();
